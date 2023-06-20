5326c86d167cf26280929e16fd567de23fa5b8d3
Samuel Lee <sam@samuels-mbp.lan>
2023-06-20 00:45:01 -0700

    a6

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+import mongoose from "mongoose";
 import express from "express";
 import cors from "cors";
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./users/users-controller.js";
-import TuitsController from "./controllers/tuits/tuits-controller.js";
+import TuitsController from "./tuits/tuits-controller.js";
 import AuthController from "./users/auth-controller.js";
 import session from "express-session";
 const app = express();
+mongoose.connect(
+  "mongodb+srv://samleefinance:testtesttest@cluster0.znlmijx.mongodb.net/tuiter?retryWrites=true&w=majority"
+);
+// mongoose.connect("mongodb://127.0.0.1:27017/tuiter");
 app.use(
   session({
     secret: "any string",
